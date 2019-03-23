0de78e834ca3a295c501a02f9bdf37cf31efce55
JosephHa99 <joeha909@gmail.com>
2019-03-23 14:02:53 -0600

    Added Q2 to main interface

--- assignment4.py
+++ assignment4.py
@@ -63,6 +63,42 @@ def q1(database_name):
 def q2(database_name):
     conn = sqlite3.connect(database_name)
     c = conn.cursor()
+    n_value = int(input("Enter number of locations:"))
+    m = folium.Map(location=[53.5444,-113.323], zoom_start=11)
+    sql_top_bot = '''
+            SELECT *
+            FROM (SELECT p.Neighbourhood_Name, c.Latitude, c.Longitude, (CANADIAN_CITIZEN+NON_CANADIAN_CITIZEN+NO_RESPONSE) as Total
+            FROM population p, coordinates c
+            WHERE p.Neighbourhood_name = c.Neighbourhood_name and total > 0 and c.Latitude != 0 and c.Longitude != 0
+            ORDER BY Total DESC LIMIT {})
+            UNION
+            SELECT *
+            FROM (SELECT p.Neighbourhood_Name, c.Latitude, c.Longitude, (CANADIAN_CITIZEN+NON_CANADIAN_CITIZEN+NO_RESPONSE) as Total
+            FROM population p, coordinates c
+            WHERE p.Neighbourhood_name = c.Neighbourhood_name and total > 0 and c.Latitude != 0 and c.Longitude != 0
+            ORDER BY Total ASC LIMIT {})
+            ORDER BY Total
+        '''.format(n_value, n_value)
+    data = pd.read_sql_query(sql_top_bot,conn)
+    for i in range(n_value):
+        folium.Circle(
+            location=[data.iloc[i]['Latitude'], data.iloc[i]['Longitude']],
+            popup=data.iloc[i]['Neighbourhood_Name'] + '\n' + str(data.iloc[i]['Total']),
+            radius = int(data.iloc[i]['Total']) * 1.1,
+            color = 'green',
+            fill = True,
+            fill_color='darkgreen',
+        ).add_to(m)
+    for i in range(n_value,len(data)):
+        folium.Circle(
+            location=[data.iloc[i]['Latitude'], data.iloc[i]['Longitude']],
+            popup=data.iloc[i]['Neighbourhood_Name'] + '\n' + str(data.iloc[i]['Total']),
+            radius = int(data.iloc[i]['Total']) * 0.1,
+            color = 'crimson',
+            fill = True,
+            fill_color='red',
+        ).add_to(m)
+    m.save('q2.html')
     conn.close()
     main_query(database_name)
 # ================================================================================
