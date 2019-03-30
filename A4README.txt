2b7a6d75549f6fe48e1799a2bf7fa50a2e930873
JosephHa99 <joeha909@gmail.com>
2019-03-30 13:35:58 -0600

    Partially fixed Q1

---
+++ A4README.txt
@@ -0,0 +1,10 @@
+Assignment #4
+
+| 		Name		|			CCID 		
+
+	Asma Omar					aaali
+	Joe Ha					    joe2
+	Debangana Ghosh				debangan
+
+Collaborated with: We did not collaborate with anyone else.
+Sources: stackoverflow.com
--- question1.py
+++ question1.py
@@ -2,20 +2,28 @@ import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
 
-crime_type = input("Enter a crime type:")
-
+conn = sqlite3.connect("./a4-sampled.db")
+c = conn.cursor()
 strt_year = int(input("Enter start year (YYYY):"))
 end_year = int(input("Enter end_year (YYYY):"))
+crime_type = input("Enter a crime type:")
 
-conn = sqlite3.connect("./dataBase.db")
-c = conn.cursor()
+q1 = '''
+    SELECT Month, Crime_Type, COUNT(Incidents_Count)
+    From crime_incidents 
+    WHERE Crime_Type = '{}' and Year >= {} and Year <= {} 
+    GROUP BY Month
+'''.format(crime_type,strt_year,end_year)
+df = pd.read_sql_query(q1, conn)
+
+"""
 c.execute("SELECT Crime_Type, Month, Year, COUNT(incidents_Count) FROM crime_incidents WHERE Crime_Type == crime_type GROUP BY Month, Crime_Type;")
 rows = c.fetchall()
 
 df = pd.read_sql_query("SELECT Crime_Type, Month, COUNT(incidents_Count) FROM crime_incidents WHERE Crime_Type == crime_type GROUP BY Month ;", conn)
 #query = '''SELECT Crime_Type, Month, COUNT(incidents_Count) FROM crime_incidents WHERE Crime_Type == crime_type GROUP BY Month ;''' %  (strt_year, end_year)
 #df = pd.read_sql_query(query, conn)
-
+"""
 print(df)
 
 plot = df.plot.bar(x="Month")
