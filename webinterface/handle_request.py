95ee315d07eca850a9eb4857028c4306cfd785f3
orackle <42990726+orackle@users.noreply.github.com>
2019-03-31 02:15:26 -0600

    Add files via upload

--- webinterface/handle_request.py
+++ webinterface/handle_request.py
@@ -10,11 +10,11 @@ def startup():
         check_connection = 'file:{}.db?mode=rw'.format(name)
         conn = sqlite3.connect(check_connection, uri=True)
         database_name = "./{}.db".format(name)
-        print("Starting up!")
+        print("Edmonton Public Crime Database")
         global dbname
         dbname = database_name
         if __name__ == '__main__':
-            app.run(debug=False)
+            app.run(debug=True)
 
     except sqlite3.OperationalError:
         #If database DNE print out error
@@ -58,42 +58,38 @@ def q2():
     if request.method == 'POST':
         conn = sqlite3.connect(dbname)
         c = conn.cursor()
-        n_value = int(request.form["number"])
+        n_areas = int(request.form["number"])
         m = folium.Map(location=[53.5444, -113.323], zoom_start=11)
         sql_top_bot = '''
-                SELECT *
-                FROM (SELECT p.Neighbourhood_Name, c.Latitude, c.Longitude, (CANADIAN_CITIZEN+NON_CANADIAN_CITIZEN+NO_RESPONSE) as Total
-                FROM population p, coordinates c
-                WHERE p.Neighbourhood_name = c.Neighbourhood_name and total > 0 and c.Latitude != 0 and c.Longitude != 0
-                ORDER BY Total DESC LIMIT {})
-                UNION
-                SELECT *
-                FROM (SELECT p.Neighbourhood_Name, c.Latitude, c.Longitude, (CANADIAN_CITIZEN+NON_CANADIAN_CITIZEN+NO_RESPONSE) as Total
-                FROM population p, coordinates c
-                WHERE p.Neighbourhood_name = c.Neighbourhood_name and total > 0 and c.Latitude != 0 and c.Longitude != 0
-                ORDER BY Total ASC LIMIT {})
-                ORDER BY Total
-            '''.format(n_value, n_value)
-        data = pd.read_sql_query(sql_top_bot,conn)
-        for i in range(n_value):
+                    SELECT p.Neighbourhood_Name, c.Latitude, c.Longitude, (CANADIAN_CITIZEN+NON_CANADIAN_CITIZEN+NO_RESPONSE) as Total
+                    FROM population p, coordinates c
+                    WHERE p.Neighbourhood_name = c.Neighbourhood_name and total > 0 and c.Latitude != 0 and c.Longitude != 0
+                    ORDER BY Total
+                '''
+        data = pd.read_sql_query(sql_top_bot, conn)
+        top_n = data.nlargest(n_areas, 'Total', keep='all')
+        bot_n = data.nsmallest(n_areas, 'Total', keep='all')
+        # print(top_n)
+        # print(bot_n)
+        for i in range(n_areas):
             folium.Circle(
-                location=[data.iloc[i]['Latitude'], data.iloc[i]['Longitude']],
-                popup=data.iloc[i]['Neighbourhood_Name'] + '\n' + str(data.iloc[i]['Total']),
-                radius = int(data.iloc[i]['Total']) * 1.1,
-                color = 'green',
-                fill = True,
-                fill_color='darkgreen',
+                location=[bot_n.iloc[i]['Latitude'], bot_n.iloc[i]['Longitude']],
+                popup=bot_n.iloc[i]['Neighbourhood_Name'] + '\n' + str(bot_n.iloc[i]['Total']),
+                radius=int(bot_n.iloc[i]['Total']) * 1.1,
+                color='green',
+                fill=True,
+                fill_color='blue',
             ).add_to(m)
-        for i in range(n_value,len(data)):
+        for i in range(n_areas):
             folium.Circle(
-                location=[data.iloc[i]['Latitude'], data.iloc[i]['Longitude']],
-                popup=data.iloc[i]['Neighbourhood_Name'] + '\n' + str(data.iloc[i]['Total']),
-                radius = int(data.iloc[i]['Total']) * 0.1,
-                color = 'crimson',
-                fill = True,
+                location=[top_n.iloc[i]['Latitude'], top_n.iloc[i]['Longitude']],
+                popup=top_n.iloc[i]['Neighbourhood_Name'] + '\n' + str(top_n.iloc[i]['Total']),
+                radius=int(top_n.iloc[i]['Total']) * 0.1,
+                color='crimson',
+                fill=True,
                 fill_color='red',
             ).add_to(m)
-        m.save('templates/q2.html')
+        m.save('q2.html')
         conn.close()
         return render_template('q2.html')
 
@@ -108,30 +104,31 @@ def q3():
         conn = sqlite3.connect(dbname)
         c = conn.cursor()
 
-        sql_statement = '''SELECT neighbourhood_name, sum(incidents_count) as total_incidents  from crime_incidents
+        sql_statement = '''SELECT neighbourhood_name, sum(incidents_count) as total_incidents  
+        from crime_incidents
         where crime_type = '%s'
         and year between %d and %d
         group by neighbourhood_name
-        order by incidents_count desc limit %d''' % (crime_type, strt_year, end_year, number_nbhd)
+        order by total_incidents desc''' % (crime_type, strt_year, end_year)
 
-        rows = pd.read_sql_query(sql_statement, conn)
+        test1 = pd.read_sql_query(sql_statement, conn)
+        rows = test1.nlargest(number_nbhd, 'total_incidents', keep='all')
 
         universe = pd.read_sql_query('''Select * from coordinates''', conn)
-        resulting = pd.merge(rows, universe, on='Neighbourhood_Name')
-        print(resulting.to_string(index=False))
-        mapped = folium.Map(location=[resulting['Latitude'].mean(), resulting['Longitude'].mean()], zoom_start=14)
+        result = pd.merge(rows, universe, on='Neighbourhood_Name')
+        print(result)
+        map = folium.Map(location=[result['Latitude'].mean(), result['Longitude'].mean()], zoom_start=14)
 
-        for row in resulting.head().itertuples():
+        for row in result.head().itertuples():
             folium.Circle(
                 location=[row.Latitude, row.Longitude],
                 popup="{} <br> {} ".format(row.Neighbourhood_Name, row.total_incidents),
-                radius=(30 * (row.total_incidents // 50)),
+                radius=(45 * (row.total_incidents // 50)),
                 color='crimson',
                 fill=True,
                 fill_color='crimson'
-            ).add_to(mapped)
-
-        mapped.save(outfile='templates/q3.html')
+            ).add_to(map)
+        map.save(outfile='q3.html')
         conn.close()
         return render_template('q3.html')
 
@@ -147,28 +144,28 @@ def q4():
         m = folium.Map(location=[53.5444,-113.323], zoom_start=11)
         c = conn.cursor()
 
-        start_statement = '''select neighbourhood_name, (canadian_citizen+ non_canadian_citizen+ no_response) as population_count from population 
+        start_statement = '''select neighbourhood_name, (canadian_citizen+ non_canadian_citizen+ no_response) as population_count 
+        from population 
         where population_count!=0
         group by neighbourhood_name;'''
-        population = pd.read_sql_query(start_statement,conn)
+        population = pd.read_sql_query(start_statement, conn)
 
-        sql_statement = '''SELECT neighbourhood_name, sum(incidents_count) as total_incidents  from crime_incidents
+        sql_statement = '''SELECT neighbourhood_name, sum(incidents_count) as total_incidents  
+        from crime_incidents
         where year between %d and %d
         group by neighbourhood_name
-        order by incidents_count''' % (strt_year, end_year)
+        order by total_incidents''' % (strt_year, end_year)
 
         crimes = pd.read_sql_query(sql_statement, conn)
 
-        crimes = pd.merge(crimes, population, on = 'Neighbourhood_Name')
-        crimes['ratio'] = crimes['total_incidents']/crimes['population_count']
+        crimes = pd.merge(crimes, population, on='Neighbourhood_Name')
+        crimes['ratio'] = crimes['total_incidents'] / crimes['population_count']
         crimes = crimes.sort_values(['ratio'], ascending=[False])
         # print(crimes.to_string(index = False))
 
         listone = ['Neighbourhood_Name', 'ratio']
-        resultfound= crimes[[col for col in listone if col in crimes.columns]]
-        resultfound = resultfound.iloc[:number_nbhd, :]
-
-
+        result = crimes[[col for col in listone if col in crimes.columns]]
+        result = result.iloc[:number_nbhd, :]
 
         parent = '''SELECT neighbourhood_name,crime_type, sum(incidents_count) as total_incidents  from crime_incidents
         where year between %d and %d
@@ -177,26 +174,26 @@ def q4():
 
         dummy = pd.read_sql_query(parent, conn)
         dummy = dummy.groupby("Neighbourhood_Name").head(1)
-        listtwo = ['Neighbourhood_Name', 'Crime_Type', 'ratio']
-        dummy = pd.merge(resultfound, dummy, on="Neighbourhood_Name")
-        semi_final=dummy[[col for col in listtwo if col in dummy.columns]]
+        list = ['Neighbourhood_Name', 'Crime_Type', 'ratio']
+        dummy = pd.merge(result, dummy, on="Neighbourhood_Name")
+        semi_final = dummy[[col for col in list if col in dummy.columns]]
 
         coordinate_query = '''select * from coordinates'''
         coord = pd.read_sql_query(coordinate_query, conn)
         coord = pd.merge(semi_final, coord, on='Neighbourhood_Name')
-        listthree = ['Neighbourhood_Name', 'Crime_Type', 'ratio', 'Latitude', 'Longitude']
-        final=coord[[col for col in listthree if col in coord.columns]]
+        list = ['Neighbourhood_Name', 'Crime_Type', 'ratio', 'Latitude', 'Longitude']
+        final = coord[[col for col in list if col in coord.columns]]
         print(final)
         for row in final.head().itertuples():
             folium.Circle(
-                location = [row.Latitude, row.Longitude],
-                popup = "{} <br> {} <br> {}".format(row.Neighbourhood_Name, row.Crime_Type, row.ratio),
-                radius = (1500*(row.ratio)),
-                color = 'crimson',
-                fill = True,
-                fill_color = 'crimson'
+                location=[row.Latitude, row.Longitude],
+                popup="{} <br> {} <br> {}".format(row.Neighbourhood_Name, row.Crime_Type, row.ratio),
+                radius=(1500 * (row.ratio)),
+                color='crimson',
+                fill=True,
+                fill_color='crimson'
             ).add_to(m)
-        m.save('templates/q4.html')
+        m.save('q4.html')
         conn.close()
         return render_template('q4.html')
 
